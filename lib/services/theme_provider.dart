ce5fc890ef3ff5b5792f9513fd1fbf55514bf195
David Oetterli <david.oetterli@hftm.ch>
2024-08-31 09:40:39 +0200

    save settings and username local

--- lib/services/theme_provider.dart
+++ lib/services/theme_provider.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider extends ChangeNotifier {
   ThemeMode _themeMode = ThemeMode.light;
 
   ThemeMode get themeMode => _themeMode;
 
-  void toggleTheme(bool isDarkMode) {
+  ThemeProvider() {
+    _loadTheme();
+  }
+
+  void toggleTheme(bool isDarkMode) async {
+    _themeMode = isDarkMode ? ThemeMode.dark : ThemeMode.light;
+    notifyListeners();
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setBool('isDarkMode', isDarkMode);
+  }
+
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final isDarkMode = prefs.getBool('isDarkMode') ?? false;
     _themeMode = isDarkMode ? ThemeMode.dark : ThemeMode.light;
     notifyListeners();
   }
--- lib/services/user_provider.dart
+++ lib/services/user_provider.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class UserProvider extends ChangeNotifier {
   String _username = 'guest';
 
   String get username => _username;
 
-  void setUsername(String newUsername) {
+  UserProvider() {
+    _loadUsername();
+  }
+
+  void setUsername(String newUsername) async {
     _username = newUsername;
+    notifyListeners();
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString('username', newUsername);
+  }
+
+  Future<void> _loadUsername() async {
+    final prefs = await SharedPreferences.getInstance();
+    _username = prefs.getString('username') ?? 'guest';
     notifyListeners();
   }
 }
